5ca3120ee231a0dd42b56b6fb92c0fcd117245c4
luohongbiao <903803342@qq.com>
2020-04-10 23:37:09 +0800

    require

---
+++ es6/16-common-js/index.js
@@ -0,0 +1,16 @@
+// var mod = require('./lib.js')
+// console.log(mod.counter)
+// mod.incCounter()
+// console.log(mod.counter)
+
+// import { counter, incCounter } from './lib.js'
+
+
+// console.log(counter)
+// incCounter()
+// console.log(counter)
+
+{/* <script type = "module">
+    import { counter, incCounter } from './lib.js';
+   
+</script> */}
---
+++ es6/16-common-js/lib.js
@@ -0,0 +1,18 @@
+// var counter = 3
+// function incCounter(){
+//     counter ++
+// }
+
+// module.exports = {
+//    get counter(){
+//        return counter
+//    },
+//     incCounter : incCounter
+// }
+
+
+export var counter = 3
+export function incCounter(){
+    counter ++
+}
+
