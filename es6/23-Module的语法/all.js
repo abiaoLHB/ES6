f77daeec75f7478f0fa9368febb5898fdba3782c
luohongbiao <hongbiao_luo@uyoung.com.cn>
2020-12-23 18:32:18 +0800

    模块

---
+++ es6/23-Module的语法/all.js
@@ -0,0 +1,4 @@
+export let name = "luohongbiao"
+export let age = 30
+export let job = 'coder'
+
---
+++ es6/23-Module的语法/module.js
@@ -0,0 +1,19 @@
+export let firstName = "Michael"
+export let lastName = "Jackson"
+export let year = 1958
+
+export function multiply(x, y) {
+    return x * y
+}
+
+export let person = {
+    name:"张三"
+}
+
+
+let address = "beijing"
+
+let nianlaing = 31
+
+
+export { address, nianlaing as age }
